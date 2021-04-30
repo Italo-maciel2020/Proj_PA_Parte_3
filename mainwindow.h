77913067eab52e110703e9a46d2568cfe5dfc49c
Anna Elizabeth <61985544+annaelizabethpsantos@users.noreply.github.com>
2021-04-30 08:04:43 -0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -11,25 +11,42 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+/*
+* Todas as operações de desenho estão nessa class
+* derivada da classe QMainWindow - herança
+* 
+*/
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+//Ponteiro para acessar e ler os dados passados pela user interface
+private:
+    Ui::MainWindow *ui;
+    int r, g, b;
 
+/*
+* Conecta o evento do movimento do slider com os slots
+* Lê um inteiro emitido pelo sinal e seta para o método
+*/
 public slots:
-    void Sair(); // interface slot, quem processa essas palavras reservadas são as máquinas declaradas no q_object
+    void Sair(); 
     void SetG(int);
     void SetR(int);
     void SetB(int);
     void pickColor();
     void updateCoords(int,int);
 
+/*
+* Métodos referentes aos icones da barra de ferramentas da interface
+* Associa um
+*/
 private slots:
    void actionSalvar();
    void actionNew();
     void actionInsertSphere();
     void actionInsertEllipsoid();
     void actionCutSphere();
-    void actionCutEllipsoid();        //Todas as funções respectivas aos icones da barra de ferramentas da interface
+    void actionCutEllipsoid();        
     void actionInsertBox();
     void actionInsertVoxel();
     void actionCutBox();

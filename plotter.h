a4ab619a91f5a20c85669a31abef1d632497581a
Italo_maciel2020 <39493130+Italo-maciel2020@users.noreply.github.com>
2021-04-30 09:42:16 -0300

    Update plotter.h

--- plotter.h
+++ plotter.h
@@ -2,15 +2,30 @@
 #define PLOTTER_H
 
 #include <QWidget>
+#include "sculptor.h"
+#include <QVector>
+#include <vector>
+
+using namespace std;
 
 class Plotter : public QWidget
 {
     Q_OBJECT
+private:
+    float fase,freq;
+    int nlinhas,ncolunas;
+   // vector <vector<voxel>> v;
+    int borderh,borderv,dimcelula,larguratela,alturatela,larguracelula,alturacelula;
+    bool pressed;    
 public:
     explicit Plotter(QWidget *parent = nullptr);
     void paintEvent(QPaintEvent *event);
+    void setTamanho(int nlinhas,int ncolunas);
+    void mousePressEvent(QMouseEvent *);
+    void mouseReleaseEvent(QMouseEvent *event);
+   // void loadMatrix(vector<vector<voxel>> a);
 signals:
-
+    void updatePos(int, int);
+public slots:
+    void setfreq(int);
 };
-
-#endif // PLOTTER_H

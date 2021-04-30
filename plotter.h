c0597ecdef79410303dc00dbcf9c259ae2b7e74c
Anna Elizabeth <61985544+annaelizabethpsantos@users.noreply.github.com>
2021-04-30 10:35:34 -0300

    Update plotter.h

--- plotter.h
+++ plotter.h
@@ -17,13 +17,16 @@ private:
    // vector <vector<voxel>> v;
     int borderh,borderv,dimcelula,larguratela,alturatela,larguracelula,alturacelula;
     bool pressed;    
+/*
+* Método que cria a caixa de desenho na interface
+*/
 public:
     explicit Plotter(QWidget *parent = nullptr);
-    void paintEvent(QPaintEvent *event);
-    void setTamanho(int nlinhas,int ncolunas);
-    void mousePressEvent(QMouseEvent *);
-    void mouseReleaseEvent(QMouseEvent *event);
-   // void loadMatrix(vector<vector<voxel>> a);
+    void paintEvent(QPaintEvent *event); // método que é ativado para o desenho na user interface a partir da classe QPaintEvent
+    void setTamanho(int nlinhas,int ncolunas); // seta o tamanho do componente de desenho
+    void mousePressEvent(QMouseEvent *); // método que gere os eventos do clique na tela
+    void mouseReleaseEvent(QMouseEvent *event); // 
+   // void loadMatrix(vector<vector<voxel>> a); // recebe uma matriz para carregar a matriz de desenho no plano, selecionando no scultor os métodos e passando para o draw
 signals:
     void updatePos(int, int);
 public slots:
